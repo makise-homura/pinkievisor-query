04c053b4e48a4015c93670d861dfbec58ae8d47e
makise-homura <akemi_homura@kurisa.ch>
2020-04-08 07:06:16 +0300

    Bump version to 0.0.4

--- pinkievisor-query.user.js
+++ pinkievisor-query.user.js
@@ -3,7 +3,7 @@
 // @description Показывает пинкивизор при клике на статистику поста или коммента
 // @include     http://tabun.everypony.ru/*
 // @include     https://tabun.everypony.ru/*
-// @version     0.0.3
+// @version     0.0.4
 // @grant       none
 // @author      makise-homura
 // ==/UserScript==
